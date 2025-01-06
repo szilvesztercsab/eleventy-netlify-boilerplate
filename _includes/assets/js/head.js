fabf20edc0de4293c335db226ba9b556ccd2b732
Csaba Szilveszter-Berei <szilvesztercsab@gmail.com>
2025-01-06 23:39:52 +0200

    fix: detect and handle prefetching accordingly

--- _includes/assets/js/head.js
+++ _includes/assets/js/head.js
@@ -8,3 +8,34 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+// Add prefetch links or speculation rules
+document.addEventListener("DOMContentLoaded", (_) => {
+  if (
+    HTMLScriptElement.supports &&
+    HTMLScriptElement.supports("speculationrules")
+  ) {
+    const specScript = document.createElement("script");
+    specScript.type = "speculationrules";
+    const specRules = {
+      prefetch: [
+        {
+          source: "document",
+          where: { href_matches: "/*" },
+        },
+      ],
+    };
+    specScript.textContent = JSON.stringify(specRules);
+    document.body.append(specScript);
+  } else {
+    Array.from(document.querySelectorAll("a"))
+      .map((a) => a.href)
+      .filter((a) => a.startsWith(document.location.origin))
+      .forEach((href) => {
+        const linkElem = document.createElement("link");
+        linkElem.rel = "prefetch";
+        linkElem.href = href;
+        document.head.append(linkElem);
+      });
+  }
+});
